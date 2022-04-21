3fcf9c2098863e3f68daf398ba7e11b670c99cb3
gtk2k <gtk2k@yahoo.co.jp>
2022-04-22 06:40:13 +0900

    commit

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -12,6 +12,11 @@ public static class BuildScript
         public string outputDir;
     }
 
+    private class BuildResult
+    {
+        public string buildResult;
+    }
+
     public static void WebGLBuild()
     {
         var paths = GetBuildScenePaths();
@@ -21,6 +26,7 @@ public static class BuildScript
         var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
         Debug.Log($"Documents Folder Path > {documentsPath}");
         var buildConfigPath = Path.Combine(documentsPath, "buildConfig.json");
+        var buildResultPath = Path.Combine(documentsPath, "buildResult.json");
         Debug.Log($"buildConfigPath > {buildConfigPath}");
         var configJson = File.ReadAllText(buildConfigPath);
         Debug.Log($"configJson > {configJson}");
@@ -39,15 +45,19 @@ public static class BuildScript
         var buildReport = BuildPipeline.BuildPlayer(buildPlayerOptions);
 
         var summary = buildReport.summary;
-       
+
+        var buildResult = new BuildResult();
         if (summary.result == UnityEditor.Build.Reporting.BuildResult.Succeeded)
         {
             Debug.Log("Success");
+            buildResult.buildResult = "success";
         }
         else
         {
             Debug.LogError("Error");
+            buildResult.buildResult = "error";
         }
+        File.WriteAllText(buildResultPath, JsonUtility.ToJson(buildResult, true));
     }
 
     private static IEnumerable<string> GetBuildScenePaths()
