012430595b26087df71692c4167b7207eb95ad6f
gtk2k <gtk2k@yahoo.co.jp>
2022-04-22 02:18:54 +0900

    commit

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -9,7 +9,7 @@ public static class BuildScript
 {
     private class BuildConfig
     {
-        public string outputDir { get; set; }
+        public string outputDir;
     }
 
     public static void WebGLBuild()
