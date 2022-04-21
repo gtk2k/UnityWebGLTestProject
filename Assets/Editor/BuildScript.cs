6c4d78d91e862b975231628ad06b3f3e7be16657
gtk2k <gtk2k@yahoo.co.jp>
2022-04-21 18:47:09 +0900

    commit

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -2,17 +2,31 @@ using UnityEditor;
 using UnityEngine;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
+using System;
 
 public static class BuildScript
 {
+    private class BuildConfig
+    {
+        public string outputDir { get; set; }
+    }
+
     public static void WebGLBuild()
     {
         var paths = GetBuildScenePaths();
         var buildOptions = BuildOptions.Development;
 
+        var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+        Debug.Log($"Documents Folder Path > {documentsPath}");
+
+        var configJson = File.ReadAllText(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "buildConfig.json"));
+        var config = JsonUtility.FromJson<BuildConfig>(configJson);
+        Debug.Log($"config > {config}");
+
         var buildReport = BuildPipeline.BuildPlayer(
             paths.ToArray(),
-            $"./WebGL_Auto_Build/{PlayerSettings.productName}",
+            $"{config.outputDir}/{PlayerSettings.productName}",
             BuildTarget.WebGL,
             buildOptions
         );
@@ -28,6 +42,7 @@ public static class BuildScript
             Debug.LogError("Error");
         }
     }
+
     private static IEnumerable<string> GetBuildScenePaths()
     {
         var scenes = new List<EditorBuildSettingsScene>(EditorBuildSettings.scenes);
